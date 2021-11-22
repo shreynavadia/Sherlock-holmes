40f484704b1d0f5a8837a2d7381ee9b96344e12b
Shrey Navadia <19it073@charusat.edu.in>
2021-11-22 07:18:48 +0530

    First commit

---
+++ sw.js
@@ -0,0 +1,74 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/cast.html',
+  '/charactermenu.css',
+  '/episodes.css',
+  '/episode.html',
+  '/menu.js',
+  '/menustyle.css',
+  '/script.js',
+  '/style.css',
+  '/images/wdicon1.png',
+  'https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i',
+  'https://i.ibb.co/3S2Bw7g/wallpaperflare-com-wallpaper-1.jpg',
+  'https://i.ibb.co/GcfGMTm/sherlock-mobile.png',
+
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
+
+const dynamicCacheName = 'site-dynamic-v1';
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key =>  key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
+    })
+  );
+});
+
+
